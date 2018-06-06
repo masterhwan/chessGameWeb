1d214f23b5b29504a29feb1185bdd42e0f5fd85b
masterhwan <mastarhwan@gmail.com>
2018-06-06 17:31:16 +0900

    준비물

---
+++ src/main/java/com/game/chess/ChessGameWebApplication.java
@@ -0,0 +1,12 @@
+package com.game.chess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessGameWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessGameWebApplication.class, args);
+	}
+}
